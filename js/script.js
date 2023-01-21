a2bafa615294021558f751346758c74aa80a6711
Nestor <nestorbryk2015@gmail.com>
2023-01-21 17:45:20 +0200

    create obj personalMovieDB

--- js/script.js
+++ js/script.js
@@ -1,5 +1,15 @@
 "use strict";
 
-const value = true;
+const numberOfFilms = prompt('Скільки фільмів ви вже подивилися ?', ''),
+      lastWachedFilm = prompt('Один з послідніх переглянутих фільмів ?', ''),
+      ratingFilm = prompt('На скільки оціните фільм ?', '');
 
-console.log(!value);
+const personalMovieDB = {
+    count: numberOfFilms,
+    movies: {},
+    actors: {},
+    genres: [],
+    privat: false,
+};
+
+personalMovieDB.movies[lastWachedFilm] = ratingFilm; 
