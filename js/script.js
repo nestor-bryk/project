e1847b0a85c281cee0debab73b670f7f32c6bba0
Nestor <nestorbryk2015@gmail.com>
2023-01-22 15:09:48 +0200

    add check count watched movies and add cycle

--- js/script.js
+++ js/script.js
@@ -1,8 +1,6 @@
 "use strict";
 
-const numberOfFilms = prompt('Скільки фільмів ви вже подивилися ?', ''),
-      lastWachedFilm = prompt('Один з послідніх переглянутих фільмів ?', ''),
-      ratingFilm = prompt('На скільки оціните фільм ?', '');
+const numberOfFilms = +prompt('Скільки фільмів ви вже подивилися ?', '');
 
 const personalMovieDB = {
     count: numberOfFilms,
@@ -12,4 +10,23 @@ const personalMovieDB = {
     privat: false,
 };
 
-personalMovieDB.movies[lastWachedFilm] = ratingFilm; 
+for(let i = 0; i < 2; i++) {
+    const a = prompt('Один з послідніх переглянутих фільмів ?', ''),
+          b = +prompt('На скільки оціните фільм ?', '');
+
+    if(a != null && b != null && a != '' && b != '' && a.length <= 50) {
+        personalMovieDB.movies[a] = b;
+    } else {
+        i--;
+    }
+}
+
+if(personalMovieDB.count <= 10) {
+    console.log("Переглянуто доволі мало фільмів!");
+} else if (personalMovieDB.count > 10 && personalMovieDB.count <= 30) {
+    console.log("Ви класичний глядач!");
+} else if (personalMovieDB.count > 30) {
+    console.log("Ви кіноман!");
+} else {
+    console.log('error');
+}
